56269d76f0491ff1c8305c89f00aefc6ac710985
Claire <claire@MacBook-Pro-de-Claire.local>
2021-10-05 15:47:20 +0200

    premier commit

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+   
+    <h3>To do List :</h3>
+    <input type="text" placeholder="Write your tasks here">
+   
+  </div>
+</template>
+
+<script>
+// import MyInp from './components/myInp.vue'
+
+export default {
+  name: 'TodoListItem',
+  components: {
+    // myInp,
+  },
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+</style>
